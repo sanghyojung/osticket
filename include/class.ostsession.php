c22cd89d1ed4881400a552a7729ba9ac6fa9892f
sanghyojung <87795311+sanghyojung@users.noreply.github.com>
2021-07-23 13:40:29 +0900

    Update class.ostsession.php

--- include/class.ostsession.php
+++ include/class.ostsession.php
@@ -322,3 +322,10 @@ class FallbackSessionBackend {
 }
 
 ?>
+
+$this->data->session_data = ""; After:
+catch (DoesNotExist $e) {
+$this->data = new SessionData(['session_id' => $id]); So it will be:
+catch (DoesNotExist $e) {
+$this->data = new SessionData(['session_id' => $id]);
+$this->data->session_data = "";
